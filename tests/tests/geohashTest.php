83bae75b9225d81cf0d993d61560d3d1ffe1c9d9
darkdarin <darkdarin@tochka.com>
2024-06-10 19:10:57 +0400

    fix: tests

--- tests/tests/geohashTest.php
+++ tests/tests/geohashTest.php
@@ -12,7 +12,7 @@ class GeoHashTest extends PHPUnit_Framework_TestCase
      */
     public function testAdjacent()
     {
-        $geohash = new Geohash();
+        $geohash = new \Tochka\GeoPHP\Adapters\GeoHash();
         $this->assertEquals('xne', $geohash->adjacent('xn7', 'top'), 'Did not find correct top adjacent geohash for xn7');
         $this->assertEquals('xnk', $geohash->adjacent('xn7', 'right'), 'Did not find correct right adjacent geohash for xn7');
         $this->assertEquals('xn5', $geohash->adjacent('xn7', 'bottom'), 'Did not find correct bottom adjacent geohash for xn7');
